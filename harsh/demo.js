8a18ad7e24c6bcd8c04e56edebba8d2b035b4a3b
Harshwardhan0831 <harshwardhanp618@gmail.com>
2025-02-03 21:17:16 -0800

    Add files via upload

---
+++ harsh/demo.js
@@ -0,0 +1,19 @@
+let  FirstNo = 20;
+let  SecondNo = 10;
+
+
+let sum = FirstNo + SecondNo;
+console.log("The Addition of "+sum);
+alert("The Addition of "+sum);
+
+let sub = FirstNo - SecondNo;
+console.log("The Subtraction of "+sub);
+alert("The Subtraction of "+sub);
+
+let mul = FirstNo * SecondNo;
+console.log("The Multiplication of "+mul);
+alert("The Multiplication of "+mul);
+
+let div = FirstNo / SecondNo;
+console.log("The Division of "+div);
+alert("The Division of "+div);
---
+++ harsh/fact.js
@@ -0,0 +1,10 @@
+let number = 5; 
+let result = 1;
+
+for (let i = 1; i <= number; i++)
+    {
+         result *= i;
+    }
+
+console.log("The Factorial is " +result);
+alert("The Factorial is " +result);
